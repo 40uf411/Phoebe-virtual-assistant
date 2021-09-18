405559b6e95d68abcf8af7425e93f80964600d43
40uf4l1 <40uf4l1@gmail.com>
2021-09-18 05:00:55 +0100

    time with city

--- plugins/plugin_time.py
+++ plugins/plugin_time.py
@@ -1,3 +1,6 @@
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+import pytz
 import datetime
 import gi
 gi.require_version('Gtk', '3.0')
@@ -8,9 +11,23 @@ from plugins.plugin import Plugin
 
 class TimePlugin(Plugin):
     def execute(self, command):
+        tokens = command.split()
+        
+        if len(tokens) < 2: # local time
+            now = datetime.datetime.now()
+            t = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
+            m = 'based on your PCs local timezone:'
+        else:
+            data = self.tzFromCity(' '.join(tokens[1:]))
+            if data == False:
+                return {"msg":"Sorry, I could not find a city with the name '%s'"%(' '.join(tokens[1:])), "content":None}
+            now = data['datetime']
+            t = data['timezone']
+            m = 'in %s.' % (' '.join(tokens[1:]))
+        
         box = gtk.Box(spacing=10, orientation="vertical")
         # Clock
-        now = datetime.datetime.now()
+        
         clockLabel = gtk.Label(
             label=now.strftime("%H : %M")
         )
@@ -18,9 +35,27 @@ class TimePlugin(Plugin):
         clockLabelContext.add_class("timePlugin_clock")
         box.add(clockLabel)
         # Timezone
-        t = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
         tLabel = gtk.Label(
             label=now.strftime("Timezone : {}".format(t))
         )
         box.add(tLabel)
-        return {"msg":"Here is the time:", "content":box}
+        return {"msg":"Here is the time %s"%(m), "content":box}
+    
+    def tzFromCity(self, city):
+        # initialize Nominatim API
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        # getting Latitude and Longitud
+        location = geolocator.geocode(city)
+        if location == None:
+            return False
+        # Time from Tz
+        tz = TimezoneFinder().timezone_at(lng=location.longitude, lat=location.latitude)
+        timeByTz = pytz.timezone(tz)
+        
+        return {
+            'location' : location,
+            'latitude' : location.latitude,
+            'longitude' : location.longitude,
+            'timezone' : tz,
+            'datetime' : datetime.datetime.now(timeByTz)
+        }
